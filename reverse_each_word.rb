05f351bd02a665bbb82de8ab385e9c3ab3a1c565
Mike Archer <mlarcher531@gmail.com>
2020-08-03 19:43:53 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word(sentence)
+new_string = []
+sentence_new = sentence.split(" ")
+  sentence_new.each do |word|
+    (new_string << word.reverse)
+  end
+final = new_string.join(" ")
+final
+end
+
+def reverse_each_word(sentence)
+new_string = []
+sentence_new = sentence.split(" ")
+  sentence_new.collect do |word|
+    (new_string << word.reverse)
+  end
+final = new_string.join(" ")
+final
+end

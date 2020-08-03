f2075888dfd86681326c80cf790d697e326488a7
Mike Archer <mlarcher531@gmail.com>
2020-08-03 19:35:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,7 @@ def reverse_each_word(sentence)
 new_string = []
 sentence_new = sentence.split(" ")
   sentence_new.each do |word|
-    new_string << word.reverse
-    new_string.join( )
+    (new_string << word.reverse).join (" ")
   end
 p new_string
 end
